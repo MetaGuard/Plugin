5e2dd81d9dfa06c866e582e2de98470cce7a4741
Vivek Nair <vivek@nair.me>
2022-07-11 20:00:20 -0700

    MetaGuard Plugin

---
+++ MetaGuard for MelonLoader/MetaGuard.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using UnityEngine;
+using UnhollowerRuntimeLib;
+using System.IO;
+using System.Reflection;
+
+namespace MetaGuard_for_MelonLoader {
+    public class MetaGuard : MelonMod {
+        AssetBundle MetaGuardAssetBundle;
+        GameObject MetaGuardPrefab;
+        GameObject MetaGuardObject;
+        GameObject VRUIObject;
+
+        public override void OnApplicationStart() {
+            LoggerInstance.Msg("MetaGuard: Loading Prefab...");
+
+            /*
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("MetaGuard_for_MelonLoader.metaguard"))
+            using (var tempStream = new MemoryStream((int)stream.Length)) {
+                stream.CopyTo(tempStream);
+                MetaGuardAssetBundle = AssetBundle.LoadFromMemory_Internal(tempStream.ToArray(), 0);
+                MetaGuardAssetBundle.hideFlags |= HideFlags.DontUnloadUnusedAsset;
+            }
+            MetaGuardPrefab = MetaGuardAssetBundle.LoadAsset_Internal("Assets/MetaGuard.prefab", Il2CppType.Of<GameObject>()).Cast<GameObject>();
+            */
+
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("MetaGuard_for_MelonLoader.metaguard"))
+            using (var tempStream = new MemoryStream((int)stream.Length)) {
+                stream.CopyTo(tempStream);
+
+                MetaGuardAssetBundle = AssetBundle.LoadFromMemory_Internal(tempStream.ToArray(), 0);
+                MetaGuardAssetBundle.hideFlags |= HideFlags.DontUnloadUnusedAsset;
+            }
+
+            MetaGuardPrefab = MetaGuardAssetBundle.LoadAsset_Internal("Assets/MetaGuard.prefab", Il2CppType.Of<GameObject>()).Cast<GameObject>();
+            MetaGuardPrefab.hideFlags |= HideFlags.DontUnloadUnusedAsset;
+
+            LoggerInstance.Msg("MetaGuard: Prefab Loaded!");
+        }
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName) {
+            LoggerInstance.Msg("MetaGuard: Inserting Prefab...");
+            LoggerInstance.Msg(MetaGuardPrefab);
+            LoggerInstance.Msg(MetaGuardPrefab.name);
+            MetaGuardObject = UnityEngine.Object.Instantiate(MetaGuardPrefab);
+            VRUIObject = MetaGuardObject.transform.GetChild(0).gameObject;
+            LoggerInstance.Msg("MetaGuard: Prefab Inserted!");
+            LoggerInstance.Msg(MetaGuardObject);
+            LoggerInstance.Msg(MetaGuardObject.name);
+        }
+
+        public override void OnUpdate() {
+            if (Input.GetKeyDown(KeyCode.T)) {
+                LoggerInstance.Msg("You just pressed T");
+
+                /*
+                GameObject VRUIMenuObject = VRUIObject.transform.GetChild(2).gameObject;
+                Camera MainCamera = Camera.main;
+                LoggerInstance.Msg(MainCamera.transform.position.ToString());
+                VRUIMenuObject.transform.position = MainCamera.transform.position + MainCamera.transform.forward * 2.5f;
+                VRUIMenuObject.transform.forward = MainCamera.transform.forward;
+                VRUIMenuObject.transform.eulerAngles = new Vector3(0, VRUIMenuObject.transform.eulerAngles.y, VRUIMenuObject.transform.eulerAngles.z);
+                VRUIMenuObject.SetActive(true);
+                */
+
+                GameObject CameraRig = Camera.main.transform.parent.parent.gameObject;
+                GameObject Left = CameraRig.transform.GetChild(0).gameObject;
+                GameObject Right = CameraRig.transform.GetChild(1).gameObject;
+                GameObject Neck = CameraRig.transform.GetChild(2).gameObject;
+
+
+            }
+        }
+    }
+}
---
+++ MetaGuard for MelonLoader/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MetaGuard for MelonLoader")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MetaGuard for MelonLoader")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: MelonInfo(typeof(MetaGuard_for_MelonLoader.MetaGuard), "MetaGuard", "1.0.0", "Vivek Nair", "https://github.com/MetaData/Plugin")]
+[assembly: MelonGame(null, null)]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1359e1ab-3b5e-4099-900a-6a097806209f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
